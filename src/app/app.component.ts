a35d11b8cf5c2e61df9e53ea1ea2a7f8b1ff2536
Rajeev Sreedharan <rajeevs1992@gmail.com>
2018-02-14 01:15:24 +0530

    entry points for emi calculators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { LoanDetails } from './models/loan-details';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public loanDetails: LoanDetails;
+
+  ngOnInit() {
+    this.loanDetails = new LoanDetails();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { LoanDetailsComponent } from './loan-details/loan-details.component';
+import { LoanRepaymentScheduleComponent } from './loan-repayment-schedule/loan-repayment-schedule.component';
+import { CommonModule } from '@angular/common';
+import { LoanSummaryComponent } from './loan-summary/loan-summary.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoanDetailsComponent,
+    LoanRepaymentScheduleComponent,
+    LoanSummaryComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
